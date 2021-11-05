05089bbd579643a639373f529e19a263cd7cfa06
hesaplattal <82821745+hesaplattal@users.noreply.github.com>
2021-11-05 22:58:11 +0300

    Update hesapla.js

--- js/hesapla.js
+++ js/hesapla.js
@@ -133,6 +133,30 @@ function hesapla() {
     document.getElementById('txtortalama').value = not1;
     document.getElementById('txtharf').value = harfnot;
 
+
+}
+
+function notifyMe() {
+
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+
+        var notification = new Notification(harfnot);
+
+    } else if (Notification.permission !== "denied") {
+
+        Notification.requestPermission().then(function(permission) {
+
+            if (permission === "granted") {
+                var notification = new Notification("Hi there!");
+            }
+
+
+        });
+
+    }
+
 }
 
 function kontrol() {

45329d239ee09c368d8b5240cf2f2bb24bc4dd89
hesaplattal <82821745+hesaplattal@users.noreply.github.com>
2021-11-05 22:59:52 +0300

    Update hesapla.js

--- js/hesapla.js
+++ js/hesapla.js
@@ -142,7 +142,7 @@ function notifyMe() {
         alert("This browser does not support desktop notification");
     } else if (Notification.permission === "granted") {
 
-        var notification = new Notification(harfnot);
+        var notification = new Notification("harfnot");
 
     } else if (Notification.permission !== "denied") {
 

349ae043606bcbbabaf06f44e4d623aa4bcc55b2
hesapla <82821745+hesapla@users.noreply.github.com>
2021-11-16 16:07:38 +0300

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-analytics.js";
-
 const firebaseConfig = {
     apiKey: "AIzaSyDiQnGQkqgbEKeHLsFjdhnok9nDOeFSkSw",
     authDomain: "hesapla-55a9f.firebaseapp.com",
